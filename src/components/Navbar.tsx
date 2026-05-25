e5f082956e4883a0ad88debcd6bafd9f28178532
Abdus Sadik Molla <sadik.only4work@gmail.com>
2026-05-25 23:56:13 +0530

    feat: update Navbar title to include 'Demo' for better branding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <nav className="dark:text-black fixed z-50 bg-white/40 backdrop-blur-2xl top-0 flex h-20 w-full items-center justify-between border px-4 text-2xl shadow-md  lg:px-20">
       <h1 className='relative text-3xl font-bold after:absolute after:bottom-2 after:inline-block after:h-3 after:w-3 after:rounded-full after:bg-emerald-400 after:content-[""] '>
-        Zustand
+        Zustand<span className='text-amber-300'>Demo</span>
       </h1>
       <ul className="hidden gap-4 md:flex">
         {NAV_ITEM.map((item) => (
